f5b315d0e682cd7d481bc9cbbe83655af2df4989
2637309949@qq.com <2637309949@qq.com>
2019-03-19 18:14:46 +0800

    结构优化

--- redis/hooks.go
+++ redis/hooks.go
@@ -1,43 +1,9 @@
+/**
+ * @author [double]
+ * @email [2637309949@qq.com]
+ * @create date 2019-03-19 17:52:35
+ * @modify date 2019-03-19 17:52:35
+ * @desc [description]
+ */
+
 package redis
-
-import (
-	"encoding/json"
-	"time"
-)
-
-// SaveToken save a token
-func (hook *Hooks) SaveToken(token map[string]interface{}) {
-	accessToken, _ := token["accessToken"]
-	refreshToken, _ := token["refreshToken"]
-	value, _ := json.Marshal(token)
-	hook.Client.Set("TOKEN:"+accessToken.(string), value, 2*24*time.Hour)
-	hook.Client.Set("TOKEN:"+refreshToken.(string), value, 5*24*time.Hour)
-}
-
-// RevokeToken revoke a token
-func (hook *Hooks) RevokeToken(accessToken string) bool {
-	status, err := hook.Client.Del("TOKEN:" + accessToken).Result()
-	if err != nil {
-		return false
-	} else if status != 1 {
-		return false
-	}
-	return true
-}
-
-// FindToken find a token
-func (hook *Hooks) FindToken(accessToken string, refreshToken string) map[string]interface{} {
-	var imapGet map[string]interface{}
-	var token string
-	if accessToken != "" {
-		token = accessToken
-	} else if refreshToken != "" {
-		token = refreshToken
-	}
-	value, err := hook.Client.Get("TOKEN:" + token).Result()
-	if err != nil {
-		return nil
-	}
-	json.Unmarshal([]byte(value), &imapGet)
-	return imapGet
-}
--- redis/redis.go
+++ redis/redis.go
@@ -1,3 +1,11 @@
+/**
+ * @author [double]
+ * @email [2637309949@qq.com]
+ * @create date 2019-03-19 17:52:35
+ * @modify date 2019-03-19 17:52:35
+ * @desc [description]
+ */
+
 package redis
 
 import (
