0db91ad4543c353a91d60321f1deb1dcad7e6d82
double <2637309949@qq.com>
2019-06-05 22:02:41 +0800

    logger优化

--- logger/multi.go
+++ logger/multi.go
@@ -1,3 +1,11 @@
+/**
+ * @author [Double]
+ * @email [2637309949@qq.com.com]
+ * @create date 2019-01-12 22:46:31
+ * @modify date 2019-01-12 22:46:31
+ * @desc [bulrush LoggerWriter plugin]
+ */
+
 package logger
 
 import (
