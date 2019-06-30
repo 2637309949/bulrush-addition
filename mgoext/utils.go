e5bcdcff4ac8be744fd9960ad027f1b03bf8aab7
double <2637309949@qq.com>
2019-06-30 18:45:55 +0800

    feat: style

--- mgoext/utils.go
+++ mgoext/utils.go
@@ -5,6 +5,8 @@
 package mgoext
 
 import (
+	"fmt"
+	"reflect"
 	"regexp"
 
 	"github.com/gin-gonic/gin"
@@ -22,4 +24,26 @@ func findStringSubmatch(matcher string, s string) []string {
 func combineHF(handler gin.HandlerFunc, handlers []gin.HandlerFunc) []gin.HandlerFunc {
 	h := append(handlers, handler)
 	return h
+}
+
+func fieldTag(target interface{}, field string, tagname string) string {
+	elementType := reflect.TypeOf(target)
+	if elementType.Kind() == reflect.Ptr {
+		elementType = elementType.Elem()
+	}
+	fmt.Println("elementType = ", elementType)
+
+	tag := ""
+	for i := 0; i < elementType.NumField(); i++ {
+		bsonFieldTag := elementType.Field(i).Tag.Get("bson")
+		if bsonFieldTag == field {
+			tag = elementType.Field(i).Tag.Get("ref")
+		}
+	}
+	if tag == "" {
+		if elementType.Field(0).Type.Kind() == reflect.Struct {
+			tag = fieldTag(reflect.New(elementType.Field(0).Type).Interface(), field, tagname)
+		}
+	}
+	return tag
 }
