09a7684e00c1c2ecf04cc1303165956580bb3ca4
double <2637309949@qq.com>
2019-06-30 14:04:16 +0800

    feat: base model

--- gorm/model.go
+++ gorm/model.go
@@ -8,8 +8,8 @@ import "time"
 
 // Model base model definition, including fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`, which could be embedded in your models
 type Model struct {
-	ID        uint       `gorm:"primary_key" bson:"id" form:"id" json:"id" xml:"id"`
-	CreatedAt time.Time  `bson:"createdAt" form:"createdAt" json:"createdAt" xml:"createdAt"`
-	UpdatedAt time.Time  `bson:"updatedAt" form:"updatedAt" json:"updatedAt" xml:"updatedAt"`
-	DeletedAt *time.Time `sql:"index" bson:"deletedAt" form:"deletedAt" json:"deletedAt" xml:"deletedAt"`
+	ID        uint       `gorm:"primary_key" form:"id" json:"id" xml:"id"`
+	CreatedAt time.Time  `form:"createdAt" json:"createdAt" xml:"createdAt"`
+	UpdatedAt time.Time  `form:"updatedAt" json:"updatedAt" xml:"updatedAt"`
+	DeletedAt *time.Time `sql:"index" form:"deletedAt" json:"deletedAt" xml:"deletedAt"`
 }
