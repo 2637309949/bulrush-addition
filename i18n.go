95fe8532d1ff1c5cb91306fee6f1569929361735
double <2637309949@qq.com>
2019-07-11 21:43:48 +0800

    fear: 国际化插件

---
+++ i18n.go
@@ -0,0 +1,116 @@
+// Copyright (c) 2018-2020 Double All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package addition
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// M defiend map
+type M map[string]interface{}
+
+// I18N defined I18N plugin
+type I18N struct {
+	Prefix    string
+	locale    string
+	locales   M
+	ctxLocale func(c *gin.Context) string
+	initFunc  func()
+}
+
+// NewI18N defined NewI18N plugin
+func NewI18N() *I18N {
+	return &I18N{
+		Prefix: "/i18n",
+		locale: "zh_CN",
+		locales: M{
+			"zh_CN": M{
+				"sys_hello": "你好",
+			},
+			"en_US": M{
+				"sys_hello": "hello",
+			},
+			"zh_TW": M{
+				"sys_hello": "妳好",
+			},
+		},
+		ctxLocale: func(c *gin.Context) string {
+			return c.Query("locale")
+		},
+	}
+}
+
+// Init i18n
+func (i18n *I18N) Init(init func(*I18N) func()) *I18N {
+	i18n.initFunc = init(i18n)
+	return i18n
+}
+
+// InitLocal defined InitLocal func
+func (i18n *I18N) InitLocal(init func(*I18N)) *I18N {
+	init(i18n)
+	return i18n
+}
+
+// Pre defined Pre func
+func (i18n *I18N) Pre() {
+	if i18n.initFunc != nil {
+		i18n.initFunc()
+	}
+}
+
+// AddLocale defined AddLocale func
+func (i18n *I18N) AddLocale(locale string, kv map[string]interface{}) *I18N {
+	i18n.locales[locale] = kv
+	return i18n
+}
+
+// SetCtxLocal defined SetCtxLocal func
+func (i18n *I18N) SetCtxLocal(ctxLocal func(c *gin.Context) string) *I18N {
+	i18n.ctxLocale = ctxLocal
+	return i18n
+}
+
+// AddLocales defined AddLocales func
+func (i18n *I18N) AddLocales(locales M) *I18N {
+	i18n.locales = locales
+	return i18n
+}
+
+// I18NLocale defined I18NLocale func
+func (i18n *I18N) I18NLocale(locale string) string {
+	return i18n.locales[i18n.locale].(M)[locale].(string)
+}
+
+// BuildI18nKey defined BuildI18nKey func
+func (i18n *I18N) BuildI18nKey(mod string, str string) string {
+	return fmt.Sprintf("%s_%s", mod, str)
+}
+
+// GetLocale defined GetLocale func
+func (i18n *I18N) GetLocale(locale string) interface{} {
+	return i18n.locales[locale]
+}
+
+// Plugin defined Plugin func
+func (i18n *I18N) Plugin(httpProxy *gin.Engine, router *gin.RouterGroup) *I18N {
+	httpProxy.Use(func(c *gin.Context) {
+		if i18n.ctxLocale(c) != "" {
+			i18n.locale = i18n.ctxLocale(c)
+		}
+		c.Next()
+	})
+	router.GET(i18n.Prefix, func(c *gin.Context) {
+		c.JSON(http.StatusOK, i18n.locales)
+	})
+	router.GET(i18n.Prefix+"/:locale", func(c *gin.Context) {
+		locale := c.Param("locale")
+		c.JSON(http.StatusOK, i18n.locales[locale])
+	})
+	return i18n
+}
